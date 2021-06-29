339b7f03b39d5d79db92d118b17b640347a73e81
LepaPiotr <piotr-lepa@o2.pl>
2021-06-29 18:07:52 +0200

    firs commit

---
+++ src/main/java/com/apzumi/apzumi/ApzumiApplication.java
@@ -0,0 +1,13 @@
+package com.apzumi.apzumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApzumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApzumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apzumi/apzumi/ApzumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apzumi.apzumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApzumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
